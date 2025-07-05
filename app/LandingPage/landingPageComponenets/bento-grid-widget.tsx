aa78266ff0194a9ca32f71e5131c64ac26178c54
abdulwahidjaved <163683209+abdulwahidjaved@users.noreply.github.com>
2025-07-06 04:00:54 +0530

    done

--- app/LandingPage/landingPageComponenets/bento-grid-widget.tsx
+++ app/LandingPage/landingPageComponenets/bento-grid-widget.tsx
@@ -1,4 +1,6 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+
 import React from "react";
 import { BentoGrid, BentoGridItem } from "./../../../components/ui/bento-grid";
 import {
@@ -7,6 +9,7 @@ import {
   IconSignature,
   IconTableColumn,
 } from "@tabler/icons-react";
+
 
 export function BentoGridSecondDemo() {
   return (
@@ -31,30 +34,58 @@ const items = [
   {
     title: "The Dawn of Innovation",
     description: "Explore the birth of groundbreaking ideas and inventions.",
-    header: <Skeleton />,
+    header: (
+      <div className="relative h-48 w-full">
+        <Image
+          src="/shield.png"
+          alt="Design"
+          fill
+          className="object-cover rounded-xl"
+        />
+      </div>
+    ),
     className: "md:col-span-2",
     icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
   },
+
   {
     title: "The Digital Revolution",
     description: "Dive into the transformative power of technology.",
-    header: <Skeleton />,
-    className: "md:col-span-1",
-    icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+    header:  (
+      <div className="relative h-48 w-full">
+        <Image
+          src="/shield.png"
+          alt="Design"
+          fill
+          className="object-scale-down rounded-xl"
+        />
+      </div>
+    ),
+  className: "md:col-span-1",
+  icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
   },
-  {
-    title: "The Art of Design",
+{
+  title: "The Art of Design",
     description: "Discover the beauty of thoughtful and functional design.",
-    header: <Skeleton />,
-    className: "md:col-span-1",
-    icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+      header: (
+      <div className="relative h-48 w-full">
+        <Image
+          src="/return-on-investment.png"
+          alt="Design"
+          fill
+          className="object-scale-down rounded-xl"
+        />
+      </div>
+    ),
+        className: "md:col-span-1",
+          icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
   },
-  {
-    title: "The Power of Communication",
+{
+  title: "The Power of Communication",
     description:
-      "Understand the impact of effective communication in our lives.",
+  "Understand the impact of effective communication in our lives.",
     header: <Skeleton />,
-    className: "md:col-span-2",
-    icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+      className: "md:col-span-2",
+        icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
   },
 ];
